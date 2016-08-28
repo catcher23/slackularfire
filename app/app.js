f699f4ccd199baeca06a66c2420ccc3215500719
Danny Lau <danlau168@gmail.com>
2016-08-27 23:29:42 -0700

    add authentication

--- app/app.js
+++ app/app.js
@@ -23,13 +23,49 @@ angular
       .state('login', {
         url: '/login',
         templateUrl: 'auth/login.html',
-        controller: 'AuthCtrl as authCtrl'
+        controller: 'AuthCtrl as authCtrl',
+        resolve: {
+          requireNoAuth: function($state, Auth) {
+            return Auth.$requireAuth().then(function(auth) {
+              $state.go('home');
+            }, function(error) {
+              return;
+            })
+          }
+        }
       })
       .state('register', {
         url: '/register',
         templateUrl: 'auth/register.html',
-        controller: 'AuthCtrl as authCtrl'
-      });
+        controller: 'AuthCtrl as authCtrl',
+        resolve: {
+          requireNoAuth: function($state, Auth) {
+            return Auth.$requireAuth().then(function(auth) {
+              $state.go('home');
+            }, function(error) {
+              return;
+            })
+          }
+        }
+      })
+      .state('profile', {
+        url: '/profile',
+        controller: 'ProfileCtrl as profileCtrl',
+        templateUrl: 'users/profile.html',
+        resolve: {
+          auth: function($state, Users, Auth) {
+            return Auth.$requireAuth().catch(function() {
+              $state.go('home');
+            })
+          },
+          profile: function(Users, Auth) {
+            return Auth.$requireAuth().then(function(auth) {
+              return Users.getProfile(auth.uid).$loaded();
+            })
+          }
+        }
+      })
+    ;
 
     $urlRouterProvider.otherwise('/');
   })
---
+++ app/users/profile.controller.js
@@ -0,0 +1,12 @@
+angular.module('angularfireSlackApp')
+  .controller('ProfileCtrl', ['$state', 'md5', 'auth', 'profile',
+    function($state, md5, auth, profile) {
+    var self = this;
+
+      self.profile = profile;
+
+      self.updateProfile = function () {
+        self.profile.emailHash = md5.createHash(auth.password.email);
+        self.profile.$save();
+      }
+  }]);
